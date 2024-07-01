2b61dc43780d91681770451fb632987121484729
matedergecz <106070991+matedergecz@users.noreply.github.com>
2024-07-01 09:31:31 +0200

    Add files via upload

---
+++ Allat2/Allat2/Program.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Allat2
+{
+    class Allat
+    {
+        //mezők
+        private string nev;
+        private int szuletesiEv;
+
+        private int szepsegPont, viselkedesPont;
+        private int pontSzam;
+        private static int aktualisEv;
+        private static int korHatar;
+
+        //konstruktor
+
+        public Allat(string nev, int szuletesiEv)
+        {
+            this.nev = nev;
+            this.szuletesiEv = szuletesiEv;
+        }
+        //metódusok
+
+        public int Kor()
+        {
+            return aktualisEv - szuletesiEv;
+        }
+        public void Pontozzak(int szepsegPont, int viselkedesPont)
+        {
+            this.szepsegPont = szepsegPont;
+            this.viselkedesPont = viselkedesPont;
+            if (Kor() <= korHatar)
+            {
+                pontSzam = viselkedesPont * Kor() + szepsegPont * (korHatar - Kor());
+            }
+            else
+            {
+                pontSzam = 0;
+            }
+        }
+
+        public override string ToString()
+        {
+            return nev + "pontszáma: " + pontSzam + "pont";
+        }
+        //tulajdonságok
+
+        //kívülről nem változtatható érték
+        public string Nev
+        {
+            get { return nev; }
+
+        }
+        public int SzuletesiEv
+        {
+            get { return szuletesiEv; }
+        }
+        public int SzepsegPont
+        {
+            get { return szepsegPont; }
+        }
+        public int ViselkedesPont
+        {
+            get { return viselkedesPont; }
+        }
+        public int PontSzam
+        {
+            get { return pontSzam; }
+        }
+        //kívülről változtatható érték
+
+        public static int AktualisEv
+        {
+            get { return aktualisEv; }
+            set { aktualisEv = value; }
+        }
+        public static int KorHatar
+        {
+            get { return KorHatar; }
+            set { korHatar = value; }
+        }
+    }
+    internal class Program
+    {
+        private static void AllatVerseny()
+        {
+            //az allat nevű változó deklarálása
+            Allat allat;
+
+            string nev;
+            int szulEv;
+            int szepseg, viselkedes;
+            int veletlenPontHatar = 10;
+
+            //egy Random példány létrehozása
+            Random rand = new Random();
+
+            // számoláshoz szükséges kezdőértékek beállítása
+            int osszesVersenyzo = 0;
+            int osszesPont = 0;
+            int legtobbPont = 0;
+
+            Console.WriteLine("Kezdődik a verseny");
+
+            char tovabb = 'i';
+            while (tovabb == 'i')
+            {
+                Console.Write("Az állat neve: ");
+                nev = Console.ReadLine();
+
+                Console.Write("születési éve: ");
+                while (!int.TryParse(Console.ReadLine(), out szulEv)
+                    || szulEv <= 0
+                    || szulEv > Allat.AktualisEv)
+                {
+                    Console.Write("Hibás adat, kérem újra");
+                }
+
+                //véletlen pontéertékek 
+                szepseg = rand.Next(veletlenPontHatar + 1);
+                viselkedes = rand.Next(veletlenPontHatar + 1);
+
+                // az állat példány létrehozása
+                allat = new Allat(nev, szulEv);
+                //a pontozási metódus meghívása
+                allat.Pontozzak(szepseg, viselkedes);
+
+                Console.WriteLine(allat);
+
+                //számítások
+
+                osszesVersenyzo++;
+                osszesPont += allat.PontSzam;
+                if (legtobbPont < allat.PontSzam)
+                {
+                    legtobbPont = allat.PontSzam;
+                }
+
+                Console.Write("Van még állat? (i/n)");
+
+                //alakítsa át ellenőrzött beolvasásra
+                tovabb = char.Parse(Console.ReadLine());
+
+            }
+            // eredmény kiíratása
+
+            Console.WriteLine("\nÖsszesen" + osszesVersenyzo + "versenyző volt, " + "\nösszpontszámuk: " + osszesPont + "pont, " + "\nlegnagyobb pontszám: " + legtobbPont);
+        }
+
+        // eredmény kiíratása
+        static void Main(string[] args)
+        {
+            int aktEv = 2015, korhatar = 10;
+            //Az aktuális év és a korhatár megadása
+            Allat.AktualisEv = aktEv;
+            Allat.KorHatar = korhatar;
+
+            AllatVerseny();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Allatfolyt/Allatfolyt/Program.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Allatfolyt
+{
+
+
+    class Allat
+    {
+        //mezők és tulajdonságok helyett automatikus tulajdonságok:
+        public string Nev { get; private set; }
+        public int SzuletesiEv { get; private set; }
+        public int RajtSzam { get; private set; }
+
+        public int SzepsegPont { get; private set; }
+        public int Viselkedespont { get; private set; }
+
+
+
+        public static int AktualisEv { get; set; }
+        public static int KorHatar { get; set; }
+
+
+        //konstruktor
+        //mivel nincsenek mezők, értékadáskor a tulajdonság kap értéket
+        public Allat(int rajtSzam, string nev, int szuletesiEv)
+        {
+            this.RajtSzam = rajtSzam;
+            this.Nev = nev;
+            this.SzuletesiEv = szuletesiEv;
+
+        }
+        //metódusok
+
+        public int Kor()
+        {
+            return AktualisEv - SzuletesiEv;
+        }
+
+        //örökölhetővé kell tenni, ezért átírjuk virtual-ra
+        //és itt is a tulajdonságokra hivatkozunk
+        public virtual int PontSzam()
+        {
+            if (Kor() < KorHatar)
+            {
+                return Viselkedespont * Kor() + SzepsegPont * (KorHatar - Kor());
+            }
+            return 0;
+        }
+        public void Pontozzak(int szepsegPont, int viselkedesPont)
+        {
+            this.SzepsegPont = szepsegPont;
+            this.Viselkedespont = viselkedesPont;
+        }
+
+        //a ToString() így kisbetűsen kiírja az osztályneveket is.
+        public override string ToString()
+        {
+            return RajtSzam + ". " + Nev + " nevű" + this.GetType().Name.ToLower()
+                + " pontszáma: " + PontSzam() + " pont";
+        }
+
+    }
+        class Kutya : Allat
+        {
+            public int GazdaViszonyPont { get; private set; }
+            public bool KapottViszonyPontot { get; private set; }
+
+            public Kutya(int rajtSzam, string nev, int szulEv) :
+                base(rajtSzam, nev, szulEv)
+            {
+            }
+
+            public void ViszonyPontozas(int gazdaViszonyPont)
+            {
+                this.GazdaViszonyPont = gazdaViszonyPont;
+                KapottViszonyPontot = true;
+            }
+            public override int PontSzam()
+            {
+                int pont = 0;
+                if (KapottViszonyPontot)
+                {
+                    pont = base.PontSzam() + GazdaViszonyPont;
+                }
+                return pont;
+            }
+
+        }
+    
+   
+    class Macska : Allat
+    {
+        public bool VanMacskaSzallitoDoboz { get; set; }
+
+        public Macska(int rajtSzam, string nev, int szulEv, bool vanMacskaSzallitoDoboz) :
+            base(rajtSzam, nev, szulEv)
+        {
+            this.VanMacskaSzallitoDoboz = vanMacskaSzallitoDoboz;
+        }
+        public override int PontSzam()
+        {
+            if (VanMacskaSzallitoDoboz)
+            {
+                return base.PontSzam();
+            }
+            return 0;
+        }
+    }
+    class Vezerles
+    {
+
+        private List<Allat> allatok = new List<Allat>();
+
+        public void Start()
+        {
+            Allat.AktualisEv = 2015;
+            Allat.KorHatar = 10;
+
+
+
+            //önállóan oldja meg a többit
+            Regisztracio();
+            Kiiratas("A regisztrált versenyzők");
+            Verseny();
+            Kiiratas("A verseny eredménye");
+        }
+
+
+        private void Regisztracio()
+        {
+            StreamReader olvasoCsatorna = new StreamReader("C:/txt/allatok.txt");
+
+            string fajta, nev;
+            int rajtSzam = 1, szulEv;
+            bool vanDoboz;
+
+            //Addig olvasunk, amíg el nem értük a fájl végét
+            while (!olvasoCsatorna.EndOfStream)
+            {
+                fajta = olvasoCsatorna.ReadLine();
+                nev = olvasoCsatorna.ReadLine();
+                szulEv = int.Parse(olvasoCsatorna.ReadLine());
+
+
+                //létrehozzuk a példányokat és hozzáadjuk az allatok listához
+                if (fajta == "kutya")
+                {
+                    allatok.Add(new Kutya(rajtSzam, nev, szulEv));
+
+                }
+                else
+                {
+                    vanDoboz = bool.Parse(olvasoCsatorna.ReadLine());
+                    allatok.Add(new Macska(rajtSzam, nev, szulEv, vanDoboz));
+                }
+                rajtSzam++;
+
+            }
+            olvasoCsatorna.Close();
+        }
+
+        private void Verseny()
+        {
+            Random rand = new Random();
+            int hatar = 11;
+            foreach (Allat item in allatok)
+            {
+                if (item is Kutya)
+                {
+                    (item as Kutya).ViszonyPontozas(rand.Next(hatar));
+                }
+                item.Pontozzak(rand.Next(hatar), rand.Next(hatar));
+            }
+        }
+        private void Kiiratas(string cim)
+        {
+            Console.WriteLine();
+            foreach (Allat item in allatok)
+            {
+                Console.WriteLine(item);
+            }
+
+
+
+        }
+    }
+
+        internal class Program
+        {
+
+            static void Main(string[] args)
+            {
+            new Vezerles().Start();
+            Console.ReadKey();
+            }
+        }
+    
+}
